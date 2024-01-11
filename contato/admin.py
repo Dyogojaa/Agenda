7893b11e01a945d79acf2d1eee61bc2b87aa61e6
DyogoJAA <dyogojaa@gmail.com>
2024-01-10 23:34:54 -0300

    Contato Admin

--- contato/admin.py
+++ contato/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+# from .models import Contact
+from contato import models
 
 # Register your models here.
+@admin.register(models.Contact)
+class ContatoAdmin(admin.ModelAdmin):
+    ...
