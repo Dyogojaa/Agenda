7adce024df8a9934084738f28711feb577ff4390
DyogoJAA <dyogojaa@gmail.com>
2024-01-11 21:36:27 -0300

    Ajustes das Classe do Modelo

--- contato/admin.py
+++ contato/admin.py
@@ -5,4 +5,16 @@ from contato import models
 # Register your models here.
 @admin.register(models.Contact)
 class ContatoAdmin(admin.ModelAdmin):
-    ...
+    list_display = ['id', 'first_name', 'last_name', 'phone']
+    ordering = ['-id']
+    list_filter = ['created_date']
+    search_fields = ['id','first_name','last_name' ]
+    list_per_page = 10
+    list_max_show_all = 200
+    # list_editable = [ 'first_name', 'last_name']
+    list_display_links = [ 'first_name', 'last_name']
+    
+    
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
